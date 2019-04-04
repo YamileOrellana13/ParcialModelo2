9f3cb5de41010b81efaeb7785fdec743c8988a6c
yamileandreaorellanasejas@gmail.com <yamileandreaorellanasejas@gmail.com>
2019-04-04 18:58:35 -0400

    Commit Producto Controller

---
+++ Parcial1.1/Parcial1.1/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace Parcial1._1.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Parcial1._1.Models.Product> Products { get; set; }
+    }
+}
---
+++ Parcial1.1/Parcial1.1/Models/Products.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1._1.Models
+{
+    public enum TipoEstado
+    {
+        Activo,
+        Inactivo
+    }
+    public enum TipoUnidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }             //damos prop y tab tab seguido y nos da toda esa propiedad
+
+        [Required]                                      //le damos control Punto en required y elimina
+        [Display(Name = "Product Name")]
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+
+        [Required]
+        public TipoUnidad Unidad { get; set; }
+
+        [Required]
+        public TipoEstado Estado { get; set; }
+
+
+    }
+}
